f3924c7aef1ecf8039de2736abc23fdfb5a36fe4
Chigozirim Nwogu <60232999+cnchigo21@users.noreply.github.com>
2023-08-13 13:58:06 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,14 +1,35 @@
-function skillsMemmber() {
-    var member = document.getElementById("member");
-    var skills = document.getElementById("skills");
-    var projects = document.getElementById("projects");
-    var contact = document.getElementById("contact");
-    var about = document.getElementById("about");
-    var home = document.getElementById("home");
-    member.style.display = "block";
-    skills.style.display = "none";
-    projects.style.display = "none";
-    contact.style.display = "none";
-    about.style.display = "none";
-    home.style.display = "none";
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var about = document.getElementById("about");
+  var memberContent = document.getElementById("memberContent");
+  var skillsContent = document.getElementById("skillsContent");
+  var projectsContent = document.getElementById("projectsContent");
+  var contactContent = document.getElementById("contactContent");
+  var aboutContent = document.getElementById("aboutContent");
+  var memberBtn = document.getElementById("memberBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+  var projectsBtn = document.getElementById("projectsBtn");
+  var contactBtn = document.getElementById("contactBtn");
+  var aboutBtn = document.getElementById("aboutBtn");
+  var memberBtn = document.getElementById("memberBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+  var projectsBtn = document.getElementById("projectsBtn");
+  var contactBtn = document.getElementById("contactBtn");
+  var aboutBtn = document.getElementById("aboutBtn");
+
+  memberBtn.addEventListener("click", function() {
+    member.classList.add("active");
+    skills.classList.remove("active");
+    projects.classList.remove("active");
+    contact.classList.remove("active");
+    about.classList.remove("active");
+    memberContent.classList.add("active");
+    skillsContent.classList.remove("active");
+    projectsContent.classList.remove("active");
+    contactContent.classList.remove("active");
+    aboutContent.classList.remove("active");
+  });
 }
