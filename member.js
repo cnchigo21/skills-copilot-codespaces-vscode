c3ffc994cfcee730fe97e021065d9867fb75cf5f
Chigozirim Nwogu <60232999+cnchigo21@users.noreply.github.com>
2023-08-13 13:40:43 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,17 +1,14 @@
 function skillsMemmber() {
-    var skills = ["HTML", "CSS", "JS", "React"];
-    var member = {
-        name: "Nguyen Van A",
-        age: 20,
-        skills: skills,
-    };
-    console.log(member.skills);
-    console.log(member.skills[1]);
-    console.log(member.skills[3]);
-    console.log(member.skills[4]);
-    console.log(member.skills.length);
-    console.log(member.skills[member.skills.length - 1]);
-    console.log(member.skills[member.skills.length - 2]);
-    console.log(member.skills[member.skills.length - 3]);
-    console.log(member.skills[member.skills.length - 4]);
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
 }
